7396d1783b95a44ef8d9cbc93c2b04c641ac9527
Luiz Scheidegger <luiz.scheidegger@gmail.com>
2011-08-18 13:12:29 -0300

    reorganization of the web server

--- web_server.py
+++ web_server.py
@@ -1,40 +1,28 @@
 import web
 import json
+import sys
         
 urls = (
     '/stats/(.*)', 'MConfStatisticsWebService'
 )
 app = web.application(urls, globals())
 
-path_prefix = './'
 class MConfStatisticsWebService:
-    URLToPathMap = {
-        'daily': path_prefix+'daily.log',
-        'weekly': path_prefix + 'weekly.log',
-        'monthly': path_prefix + 'monthly.log'
-    }
-
-    def parseLog(self, filename):
-        f = open(filename, 'r')
-        lines = f.readlines()
-        f.close()
-
-        data = {}
-        data['points'] = []
-        for line in lines:
-            tokens = line.split()
-            data['points'].append([float(tokens[0]), float(tokens[1])])
-
-        return data
-
     def GET(self, window):
         if not window:
             window = 'monthly'
 
-        filename = MConfStatisticsWebService.URLToPathMap[window]
+        obj = json.loads(file(sys.argv[2]).read())[window]
 
-        data = self.parseLog(MConfStatisticsWebService.URLToPathMap[window])
-        return json.dumps(data)
+        # we take the first datapoint as a template for all others
+        output = {}
+        keys = obj['datapoints'][0]['value'].keys()
+        for key in keys:
+            output[key] = []
+            for datapoint in obj['datapoints']:
+                output[key].append([datapoint['timestamp'], datapoint['value'][key]])
+
+        return json.dumps(output)
 
 if __name__ == "__main__":
     app.run()
