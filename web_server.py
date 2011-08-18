639cfc7c331699fcbfdf128ddddd040e12fb08d7
Luiz Scheidegger <luiz.scheidegger@gmail.com>
2011-08-18 10:51:51 -0300

    web_server now returns a JSON object

--- web_server.py
+++ web_server.py
@@ -1,15 +1,40 @@
 import web
+import json
         
 urls = (
-    '/(.*)', 'hello'
+    '/stats/(.*)', 'MConfStatisticsWebService'
 )
 app = web.application(urls, globals())
 
-class hello:        
-    def GET(self, name):
-        if not name: 
-            name = 'World'
-        return 'Hello, ' + name + '!'
+path_prefix = './'
+class MConfStatisticsWebService:
+    URLToPathMap = {
+        'daily': path_prefix+'daily.log',
+        'weekly': path_prefix + 'weekly.log',
+        'monthly': path_prefix + 'monthly.log'
+    }
+
+    def parseLog(self, filename):
+        f = open(filename, 'r')
+        lines = f.readlines()
+        f.close()
+
+        data = {}
+        data['points'] = []
+        for line in lines:
+            tokens = line.split()
+            data['points'].append([float(tokens[0]), float(tokens[1])])
+
+        return data
+
+    def GET(self, window):
+        if not window:
+            window = 'monthly'
+
+        filename = MConfStatisticsWebService.URLToPathMap[window]
+
+        data = self.parseLog(MConfStatisticsWebService.URLToPathMap[window])
+        return json.dumps(data)
 
 if __name__ == "__main__":
     app.run()
