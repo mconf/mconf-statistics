6d48165da9206ed22589276f15780805a4af464b
Luiz Scheidegger <luiz.scheidegger@gmail.com>
2011-08-19 13:28:33 -0300

    web server now serves the full JSON object by default

--- web_server.py
+++ web_server.py
@@ -9,20 +9,25 @@ app = web.application(urls, globals())
 
 class MConfStatisticsWebService:
     def GET(self, window):
-        i = web.input(window='monthly', callback='(function(obj){})')
+        i = web.input(window='all', callback='(function(obj){})')
         window = i.window
         callback = i.callback
         web.header('Content-Type', 'application/x-javascript')
 
-        obj = json.loads(file(sys.argv[2]).read())[window]
-
-        # we take the first datapoint as a template for all others
         output = {}
-        keys = obj['datapoints'][0]['value'].keys()
-        for key in keys:
-            output[key] = []
-            for datapoint in obj['datapoints']:
-                output[key].append([datapoint['timestamp'], datapoint['value'][key]])
+
+        if window == 'all':
+            output = json.loads(file(sys.argv[2]).read())
+            
+        else:
+            obj = json.loads(file(sys.argv[2]).read())[window]
+
+            # we take the first datapoint as a template for all others
+            keys = obj['datapoints'][0]['value'].keys()
+            for key in keys:
+                output[key] = []
+                for datapoint in obj['datapoints']:
+                    output[key].append([datapoint['timestamp'], datapoint['value'][key]])
 
         # this is kinda bad, but it's what we need to implement
         # jsonp, otherwise it wouldn't work
